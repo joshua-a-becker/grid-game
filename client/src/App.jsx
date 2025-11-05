ba3ddfc3381a3d559b2f4dc633726671000e70bc
Joshua Aaron Becker <joshuabecker@Joshuas-MacBook-Air.local>
2025-11-05 20:59:21 +0000

    dev URL and server setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,35 @@
 import { EmpiricaClassic } from "@empirica/core/player/classic";
 import { EmpiricaContext } from "@empirica/core/player/classic/react";
 import { EmpiricaMenu, EmpiricaParticipant } from "@empirica/core/player/react";
-import React from "react";
+import React, { useEffect } from "react";
 import { Game } from "./Game";
 import { ExitSurvey } from "./intro-exit/ExitSurvey";
 import { Introduction } from "./intro-exit/Introduction";
 import { AutoPlayerIdForm } from "./intro-exit/AutoPlayerIdForm";
 
+// Generate random 20-character alphanumeric string
+function generateParticipantKey() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 20; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
 export default function App() {
   const urlParams = new URLSearchParams(window.location.search);
   const playerKey = urlParams.get("participantKey") || "";
+
+  // If no participantKey, generate one and redirect
+  useEffect(() => {
+    if (!playerKey) {
+      const newKey = generateParticipantKey();
+      urlParams.set("participantKey", newKey);
+      const newUrl = `${window.location.pathname}?${urlParams.toString()}`;
+      window.location.replace(newUrl);
+    }
+  }, [playerKey, urlParams]);
 
   const { protocol, host } = window.location;
   const url = `${protocol}//${host}/query`;
